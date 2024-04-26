122af06c81e98251d5a1fd25586dcf5b63f9a813
huzair <huzairahmedkhan@gmail.com>
2024-04-26 22:08:48 +0500

    learnt oop

---
+++ 10-classes-and-oop/instanceOf.js
@@ -0,0 +1,20 @@
+// function car (make,model,year){
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+
+// }
+
+// const auto = new car("honda",'city',2024);
+
+function a (){}
+function b (){
+}
+
+const c = new a();
+
+console.log(c instanceof a);
+console.log(c instanceof b);
+
+// console.log(auto instanceof car);
+// console.log(auto instanceof Object);
